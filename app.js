49c3fb004fc714ba52d8c9faf317dd3a5b8c8a3d
Gabriel <dicierigabriel@gmail.com>
2020-08-08 22:18:16 -0300

    1.0.2

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 $(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
+  $(window).on("scroll", function () {
+    var w = window.innerWidth;
+    if ($(window).scrollTop() > 10) {
+      
+      document.getElementById("logo").src = "src/fire_escudary.png";
+      $(".navbar").addClass("active");
+    } 
+    else {
+      if (w>992){
+      document.getElementById("logo").src = "src/fire_escuudary_light.png";
+    }
+      $(".navbar").removeClass("active");
+    }
+  });
 });
+
+
+$( window ).on( "load", function() {
+  var w = window.innerWidth;
+  if (w<992){
+  console.log(w);
+  document.getElementById("logo").src = "src/fire_escudary.png";
+}
+});
+
+$(document).ready(function() { 
+  $(window).resize(function() { 
+    var w = window.innerWidth;
+    if (w<992){
+      console.log(w);
+      document.getElementById("logo").src = "src/fire_escudary.png";
+    }
+    else{
+      document.getElementById("logo").src = "src/fire_escuudary_light.png";
+    }
+  }); 
+}); 
